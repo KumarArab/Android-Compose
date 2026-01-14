e1b7e5b1dc6fc25c758c622de177dceaa7b3a70a
Arab Kumar <arabkumar1000@gmail.com>
2026-01-14 15:00:39 +0530

    added unit and intrumentation tests for tip calculator

--- TipCalculator/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ TipCalculator/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -170,7 +171,8 @@ fun EditNumberField(
  * according to the local currency.ÒÒÒÒ
  * Example would be "$10.00".
  */
-private fun calculateTip(
+@VisibleForTesting
+internal fun calculateTip(
     amount: Double,
     tipPercent: Double = 15.0,
     roundUp: Boolean = false
